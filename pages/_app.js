4a4a93ed253fc582950d7f5685ce1370c8303117
Larry <104570219+Larry-Master@users.noreply.github.com>
2025-09-26 00:36:41 +0200

    Test

--- pages/_app.js
+++ pages/_app.js
@@ -42,6 +42,9 @@ export default function App({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
         <meta name="theme-color" content="#3B82F6" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <DarkModeToggle />
       <ErrorBoundary>
