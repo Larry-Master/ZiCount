d71745575ae7f009dedc46af4142101ad352499a
Larry <104570219+Larry-Master@users.noreply.github.com>
2025-09-28 11:52:22 +0200

    Revert "Update to Claim"
    This reverts commit 485e080b46b50d6240d52af93fa796b5ee7c712b.

--- components/ReceiptDetail.js
+++ components/ReceiptDetail.js
@@ -75,10 +75,7 @@ export default function ReceiptDetail({ receipt, receiptId, currentUserId, onIte
 
   const getItemStatus = (item) => {
     const optimistic = optimisticClaims?.get ? optimisticClaims.get(item.id) : undefined;
-    // optimistic may only contain claim metadata; merge it onto the full item so components
-    // receive a complete object (name, price, etc.) while overriding claim fields.
-    if (optimistic) return { ...item, ...optimistic };
-    return item;
+    return optimistic || item;
   };
 
   // If editing manual receipt, show the form
